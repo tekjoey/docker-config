1117cb0376e31a6130f4834230c7c40c9cefca4f
Josiah <josiah.mckay@protonmail.com>
2026-03-12 20:09:45 -0400

    updated docker networks to seperate DBs and sidecars from proxy network

--- infra/create-networks.sh
+++ infra/create-networks.sh
@@ -1,4 +1,8 @@
 # Bash script to create all neccessary Docker networks
 
 docker network create proxy
-
+docker network create auth
+docker network create immich
+docker network create nextcloud
+docker network create paperless
+docker network create vikunja
