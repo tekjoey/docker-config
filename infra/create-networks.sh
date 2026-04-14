f0a0b78f84f3d5d351ac49f18995097d0a7edfc9
Josiah <josiah.mckay@protonmail.com>
2026-04-13 21:17:32 -0400

    started using ansible. Create playbook for docker networks and PYTHONPATH

--- infra/create-networks.sh
+++
@@ -1,8 +0,0 @@
-# Bash script to create all neccessary Docker networks
-
-docker network create proxy
-docker network create auth
-docker network create immich
-docker network create nextcloud
-docker network create paperless
-docker network create vikunja
